486dcb3f814a4477c2c575b6522628ba9bc73b86
Martin Rylund <martin@rylund.eu>
2019-05-11 19:46:13 +0200

    Opdateret productbatch DAO interface

--- src/main/java/dal/IProductBatchDAO.java
+++ src/main/java/dal/IProductBatchDAO.java
@@ -1,15 +1,16 @@
 package dal;
 
 import dto.IProductBatchDTO;
+import dto.IUserDTO;
 
 import java.util.List;
 
 public interface IProductBatchDAO {
-    void addproductBatch(IProductBatchDTO product, int userID) throws MySQL_conn.DALException;
+    void addproductBatch(IProductBatchDTO product, IUserDTO admin) throws MySQL_conn.DALException;
 
     IProductBatchDTO getProductBatch(int id) throws MySQL_conn.DALException;
 
     List<IProductBatchDTO> getProductBatches() throws MySQL_conn.DALException;
 
-    void deleteProductBatch(int productID, int userID) throws MySQL_conn.DALException;
+    void deleteProductBatch(int productID, IUserDTO admin) throws MySQL_conn.DALException;
 }
